c23a7123cda105ff204012d46b879be19c61e769
eugeneoei <eugeneoei89@gmail.com>
2022-07-16 10:10:55 +0800

    added header and footer partials into home.ejs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 require("dotenv").config();
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("home");
